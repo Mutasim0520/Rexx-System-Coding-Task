e028f75aa254e87f8fd9ec44301972084d018cfb
Mutasim0520 <mutasimfuad0520@gmail.com>
2020-06-04 14:43:47 +0100

    bootstrap added

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,52 +1,68 @@
 @extends('layouts.layout')
 @section('content')
-    <div>
-        Welcome
+    <div class='col-md-12'>
+        <h1 class='display-2'>Welcome</h1>
+    </div>
+    <div class="col-md-12">
         <form enctype="multipart/form-data" method="post" action="{{Route('store.json')}}">
         {{csrf_field()}}
         Upload JSON file <input type ='file' name='json_file'>
-        <input type='submit' value = 'Submit'>
+        <buton class="btn btn-primary" type='submit'>Upload</button>
         </form>
     </div>
-    <div>
-        <form enctype="multipart/form-data" method="get" action="{{Route('filter')}}">
-        {{csrf_field()}}
-        <span>Enter Customer Name</span>
-        <input type = 'text' name = 'customer_name' placeholder='John Doe'>
-        <span>Enter Product Name</span>
-        <input type = 'text' name = 'product_name' placeholder=''>
-        <span>Enter Product Price</span>
-        <input type = 'text' name = 'product_price' placeholder='34.99'>
-        <input type = 'submit' value = 'Filter'>
+    <div class='col-md-12'>
+    <h1 class='display-5'>Filter your data</h1>
+        <form class = 'form' enctype="multipart/form-data" method="get" action="{{Route('filter')}}">
+            {{csrf_field()}}
+            <div class="form-group col-md-4">
+                <span>Enter Customer Name</span>
+                <input class="form-control" type = 'text' name = 'customer_name' placeholder='John Doe'>
+            </div>
+            <div class="form-group col-md-4">
+                <span>Enter Product Name</span>
+                <input class="form-control" type = 'text' name = 'product_name' placeholder=''>
+            </div>
+            <div class="form-group col-md-4">
+                <span>Enter Product Price</span>
+                <input class="form-control" type = 'text' name = 'product_price' placeholder='34.99'>
+            </div>
+            <div class='form-group col-md-3'>
+                <buton class="btn btn-primary" type='submit'>Filter</button>
+            </div>
         </form>
     </div>
     @if($query_result)
-        <div>
-            <table>
-                <tr>
-                    <th>Date</th>
-                    <th>Customer Name</th>
-                    <th>Customer Email</th>
-                    <th>Product Name</th>
-                    <th>Product ID</th>
-                    <th>price</th>
-                </tr>
-                <?php $total = 0; ?>
-                @foreach($query_result as $result)
+        <div class='col-md-12'>
+        <h1 class='display-5'>Filter Result</h1>
+            <table class='table'>
+                <thead>
                     <tr>
-                        <td>{{$result->date}}</td>
-                        <td>{{$result->customer->name}}</td>
-                        <td>{{$result->customer->email}}</td>
-                        <td>{{$result->product->name}}</td>
-                        <td>{{$result->product->id}}</td>
-                        <td>{{$result->product->price}}</td>
+                        <th>Date</th>
+                        <th>Customer Name</th>
+                        <th>Customer Email</th>
+                        <th>Product Name</th>
+                        <th>Product ID</th>
+                        <th>price</th>
                     </tr>
-                    <?php $total = $total+floatval($result->product->price); ?>
-                @endforeach
-                <tr>
-                    <td colspan=5>Total</td>
-                    <td>{{$total}}</td>
-                </tr>
+                </thead>
+                <tbody>
+                    <?php $total = 0; ?>
+                    @foreach($query_result as $result)
+                        <tr>
+                            <td>{{$result->date}}</td>
+                            <td>{{$result->customer->name}}</td>
+                            <td>{{$result->customer->email}}</td>
+                            <td>{{$result->product->name}}</td>
+                            <td>{{$result->product->id}}</td>
+                            <td>{{$result->product->price}}</td>
+                        </tr>
+                        <?php $total = $total+floatval($result->product->price); ?>
+                    @endforeach
+                    <tr>
+                        <td colspan=5>Total</td>
+                        <td>{{$total}}</td>
+                    </tr>
+                </tbody>
             </table>
         </div>
     @endif
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
 <head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
     @yield('styles')
 </head>
 <body>
-    <div class="mainbody background">
+    <div class="container">
         @yield('content')
     </div>
     
